0d4a06c61e89b87d776bf5d156e78a61267077dd
toctoce <toctoce@naver.com>
2026-01-04 14:51:42 +0900

    feat(date): 올바른 입력 검증 기능 구현

--- src/main/java/oncall/vo/date/Date.java
+++ src/main/java/oncall/vo/date/Date.java
@@ -1,10 +1,14 @@
 package oncall.vo.date;
 
+import static oncall.common.ErrorMessage.INVALID_INPUT;
+
 import java.time.DayOfWeek;
 import java.time.format.TextStyle;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Pattern;
+import oncall.common.OncallException;
 
 public record Date(int month, int dayOfMonth, DayOfWeek dayOfWeek, boolean isHoliday) {
     static final int[] MAX_DAY_OF_MONTH = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
@@ -23,6 +27,54 @@ public record Date(int month, int dayOfMonth, DayOfWeek dayOfWeek, boolean isHol
         this(month, dayOfMonth, dayOfWeek, HOLIDAYS.contains(List.of(month, dayOfMonth)));
     }
 
+    public static Date from(String input) {
+        validate(input);
+
+        String[] split = input.split(",");
+        int month = Integer.parseInt(split[0]);
+        String dayOfWeek = split[1];
+
+        return Date.from(month, dayOfWeek);
+    }
+
+    private static void validate(String input) {
+        validateFormat(input);
+
+        String[] split = input.split(",");
+        int month = Integer.parseInt(split[0]);
+        validateMonth(month);
+
+        String dayOfWeek = split[1];
+        validateDayOfWeek(dayOfWeek);
+    }
+
+    private static void validateDayOfWeek(String dayOfWeek) {
+        List<String> dayOfWeeks = List.of("월", "화", "수", "목", "금", "토", "일");
+        if (!List.of("월", "화", "수", "목", "금", "토", "일").contains(dayOfWeek)) {
+            throw new OncallException("올바른 요일을 입력하세요.");
+        }
+    }
+
+    private static void validateMonth(int month) {
+        if (month < 1 || month > 12) {
+            throw new OncallException("올바른 월을 입력하세요.");
+        }
+    }
+
+
+    private static void validateFormat(String input) {
+        if (!Pattern.matches("[0-9]{1,2},[ㄱ-ㅎ가-힣]{1}", input)) {
+            throw new OncallException(INVALID_INPUT.getMessage());
+        }
+    }
+
+    private static Date from(int month, String dayOfWeek) {
+        List<String> dayOfWeeks = List.of("", "월", "화", "수", "목", "금", "토", "일");
+        int dayOfWeekIndex = dayOfWeeks.indexOf(dayOfWeek);
+
+        return new Date(month, 1, DayOfWeek.of(dayOfWeekIndex));
+    }
+
     public Date nextDate() {
         if (dayOfMonth == MAX_DAY_OF_MONTH[month]) {
             return null;
@@ -36,6 +88,11 @@ public record Date(int month, int dayOfMonth, DayOfWeek dayOfWeek, boolean isHol
 
     @Override
     public String toString() {
-        return String.format("%d월 %d일 %s", month, dayOfMonth, dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN));
+        String str = String.format("%d월 %d일 %s", month, dayOfMonth,
+                dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREAN));
+        if (dayOfWeek != DayOfWeek.SATURDAY && dayOfWeek != DayOfWeek.SUNDAY && isHoliday) {
+            str += "(휴일)";
+        }
+        return str;
     }
 }
